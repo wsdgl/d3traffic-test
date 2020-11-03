ea17c7b6efe1ed188289151a7df663f3acaf97e6
wuhao1 <wuhao1@supcon.com>
2020-11-03 16:50:52 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
+<!--
+ * @Author: wuhao
+ * @Date: 2020-11-03 16:00:13
+ * @LastEditors: wuhao
+ * @LastEditTime: 2020-11-03 16:13:24
+ * @Description: description
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
+/*
+ * @Author: wuhao
+ * @Date: 2020-11-03 16:00:13
+ * @LastEditors: wuhao
+ * @LastEditTime: 2020-11-03 16:08:28
+ * @Description: description
+ */
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import { VuePlugin } from 'vuera'
+ 
+Vue.use(VuePlugin)
 
 new Vue({
   render: h => h(App),
